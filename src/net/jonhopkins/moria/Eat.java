80c684349b705c4f9995da30f4ded3f32cf0f165
Jon Hopkins <jhopkins720@gmail.com>
2017-03-07 20:45:56 -0500

    Refactor method in Eat
    - declare variables in smallest possible scope
    - reduce complexity where possible
    - move foods' effects on player into separate methods

--- src/net/jonhopkins/moria/Eat.java
+++ src/net/jonhopkins/moria/Eat.java
@@ -21,7 +21,6 @@
  */
 package net.jonhopkins.moria;
 
-import net.jonhopkins.moria.types.PlayerFlags;
 import net.jonhopkins.moria.types.IntPointer;
 import net.jonhopkins.moria.types.InvenType;
 import net.jonhopkins.moria.types.PlayerMisc;
@@ -30,160 +29,220 @@ public class Eat {
 	
 	private Eat() { }
 	
-	/* Eat some food.					-RAK-	*/
+	/**
+	 * Eat some food. -RAK-
+	 */
 	public static void eat() {
-		IntPointer i = new IntPointer();
-		IntPointer j = new IntPointer(), k = new IntPointer(), item_val = new IntPointer();
-		boolean ident;
-		PlayerFlags f_ptr;
-		PlayerMisc m_ptr;
-		InvenType i_ptr;
-		
 		Variable.freeTurnFlag = true;
 		if (Treasure.invenCounter == 0) {
 			IO.printMessage("But you are not carrying anything.");
-		} else if (!Misc3.findRange(Constants.TV_FOOD, Constants.TV_NEVER, j, k)) {
-			IO.printMessage("You are not carrying any food.");
-		} else if (Moria1.getItemId(item_val, "Eat what?", j.value(), k.value(), "", "")) {
-			i_ptr = Treasure.inventory[item_val.value()];
-			Variable.freeTurnFlag = false;
-			i.value(i_ptr.flags);
-			ident = false;
-			while (i.value() != 0) {
-				j.value(Misc1.firstBitPos(i) + 1);
-				/* Foods					*/
-				switch(j.value()) {
-					case 1:
-						f_ptr = Player.py.flags;
-						f_ptr.poisoned += Misc1.randomInt(10) + i_ptr.level;
-						ident = true;
-						break;
-					case 2:
-						f_ptr = Player.py.flags;
-						f_ptr.blind += Misc1.randomInt(250) + 10 * i_ptr.level + 100;
-						Misc3.drawCave();
-						IO.printMessage("A veil of darkness surrounds you.");
-						ident = true;
-						break;
-					case 3:
-						f_ptr = Player.py.flags;
-						f_ptr.afraid += Misc1.randomInt(10) + i_ptr.level;
-						IO.printMessage("You feel terrified!");
-						ident = true;
-						break;
-					case 4:
-						f_ptr = Player.py.flags;
-						f_ptr.confused += Misc1.randomInt(10) + i_ptr.level;
-						IO.printMessage("You feel drugged.");
-						ident = true;
-						break;
-					case 5:
-						f_ptr = Player.py.flags;
-						f_ptr.imagine += Misc1.randomInt(200) + 25 * i_ptr.level + 200;
-						IO.printMessage("You feel drugged.");
-						ident = true;
-						break;
-					case 6:
-						ident = Spells.curePoison();
-						break;
-					case 7:
-						ident = Spells.cureBlindness();
-						break;
-					case 8:
-						f_ptr = Player.py.flags;
-						if (f_ptr.afraid > 1) {
-							f_ptr.afraid = 1;
-							ident = true;
-						}
-						break;
-					case 9:
-						ident = Spells.cureConfusion();
-						break;
-					case 10:
-						ident = true;
-						Spells.loseStrength();
-						break;
-					case 11:
-						ident = true;
-						Spells.loseConstitution();
-						break;
-					case 16:
-						if (Misc3.restoreStat(Constants.A_STR)) {
-							IO.printMessage("You feel your strength returning.");
-							ident = true;
-						}
-						break;
-					case 17:
-						if (Misc3.restoreStat(Constants.A_CON)) {
-							IO.printMessage("You feel your health returning.");
-							ident = true;
-						}
-						break;
-					case 18:
-						if (Misc3.restoreStat(Constants.A_INT)) {
-							IO.printMessage("Your head spins a moment.");
-							ident = true;
-						}
-						break;
-					case 19:
-						if (Misc3.restoreStat(Constants.A_WIS)) {
-							IO.printMessage("You feel your wisdom returning.");
-							ident = true;
-						}
-						break;
-					case 20:
-						if (Misc3.restoreStat(Constants.A_DEX)) {
-							IO.printMessage("You feel more dextrous.");
-							ident = true;
-						}
-						break;
-					case 21:
-						if (Misc3.restoreStat(Constants.A_CHR)) {
-							IO.printMessage("Your skin stops itching.");
-							ident = true;
-						}
-						break;
-					case 22:
-						ident = Spells.changePlayerHitpoints(Misc1.randomInt(6));
-						break;
-					case 23:
-						ident = Spells.changePlayerHitpoints(Misc1.randomInt(12));
-						break;
-					case 24:
-						ident = Spells.changePlayerHitpoints(Misc1.randomInt(18));
-						break;
-					case 26:
-						ident = Spells.changePlayerHitpoints(Misc1.damageRoll(3, 12));
-						break;
-					case 27:
-						Moria1.takeHit(Misc1.randomInt(18), "poisonous food.");
-						ident = true;
-						break;
-					default:
-						IO.printMessage("Internal error in eat()");
-						break;
-				}
-				/* End of food actions.				*/
-			}
-			if (ident) {
-				if (!Desc.isKnownByPlayer(i_ptr)) {
-					/* use identified it, gain experience */
-					m_ptr = Player.py.misc;
-					/* round half-way case up */
-					m_ptr.currExp += (i_ptr.level + (m_ptr.level >> 1)) / m_ptr.level;
-					Misc3.printExperience();
-					
-					Desc.identify(item_val);
-					i_ptr = Treasure.inventory[item_val.value()];
-				}
-			} else if (!Desc.isKnownByPlayer(i_ptr)) {
-				Desc.sample(i_ptr);
-			}
-			Misc1.addFood(i_ptr.misc);
-			Player.py.flags.status &= ~(Constants.PY_WEAK | Constants.PY_HUNGRY);
-			Misc3.printHunger();
-			Desc.describeRemaining(item_val.value());
-			Misc3.destroyInvenItem(item_val.value());
+			return;
 		}
+		
+		IntPointer first = new IntPointer();
+		IntPointer last = new IntPointer();
+		if (!Misc3.findRange(Constants.TV_FOOD, Constants.TV_NEVER, first, last)) {
+			IO.printMessage("You are not carrying any food.");
+			return;
+		}
+		
+		IntPointer item_val = new IntPointer();
+		if (!Moria1.getItemId(item_val, "Eat what?", first.value(), last.value(), "", "")) {
+			return;
+		}
+		
+		InvenType food = Treasure.inventory[item_val.value()];
+		Variable.freeTurnFlag = false;
+		IntPointer flags = new IntPointer(food.flags);
+		boolean identified = false;
+		while (flags.value() != 0) {
+			int foodType = Misc1.firstBitPos(flags) + 1;
+			// Foods
+			switch (foodType) {
+			case 1:
+				identified = poisonPlayer(food);
+				break;
+			case 2:
+				identified = blindPlayer(food);
+				break;
+			case 3:
+				identified = scarePlayer(food);
+				break;
+			case 4:
+				identified = confusePlayer(food);
+				break;
+			case 5:
+				identified = hallucinatePlayer(food);
+				break;
+			case 6:
+				identified = Spells.curePoison();
+				break;
+			case 7:
+				identified = Spells.cureBlindness();
+				break;
+			case 8:
+				identified = cureConfusion();
+				break;
+			case 9:
+				identified = Spells.cureConfusion();
+				break;
+			case 10:
+				identified = true;
+				Spells.loseStrength();
+				break;
+			case 11:
+				identified = true;
+				Spells.loseConstitution();
+				break;
+			case 16:
+				identified = restoreStrength();
+				break;
+			case 17:
+				identified = restoreConstitution();
+				break;
+			case 18:
+				identified = restoreIntelligence();
+				break;
+			case 19:
+				identified = restoreWisdom();
+				break;
+			case 20:
+				identified = restoreDexterity();
+				break;
+			case 21:
+				identified = restoreCharisma();
+				break;
+			case 22:
+				identified = Spells.changePlayerHitpoints(Misc1.randomInt(6));
+				break;
+			case 23:
+				identified = Spells.changePlayerHitpoints(Misc1.randomInt(12));
+				break;
+			case 24:
+				identified = Spells.changePlayerHitpoints(Misc1.randomInt(18));
+				break;
+			case 26:
+				identified = Spells.changePlayerHitpoints(Misc1.damageRoll(3, 12));
+				break;
+			case 27:
+				identified = damagePlayer();
+				break;
+			default:
+				IO.printMessage("Internal error in eat()");
+				break;
+			}
+		}	
+		
+		if (identified) {
+			if (!Desc.isKnownByPlayer(food)) {
+				// use identified it, gain experience
+				// round half-way case up
+				PlayerMisc misc = Player.py.misc;
+				misc.currExp += (food.level + (misc.level >> 1)) / misc.level;
+				Misc3.printExperience();
+				
+				Desc.identify(item_val);
+				food = Treasure.inventory[item_val.value()];
+			}
+		} else if (!Desc.isKnownByPlayer(food)) {
+			Desc.sample(food);
+		}
+		
+		Misc1.addFood(food.misc);
+		Player.py.flags.status &= ~(Constants.PY_WEAK | Constants.PY_HUNGRY);
+		Misc3.printHunger();
+		Desc.describeRemaining(item_val.value());
+		Misc3.destroyInvenItem(item_val.value());
+	}
+	
+	private static boolean poisonPlayer(InvenType food) {
+		Player.py.flags.poisoned += Misc1.randomInt(10) + food.level;
+		return true;
+	}
+	
+	private static boolean blindPlayer(InvenType food) {
+		Player.py.flags.blind += Misc1.randomInt(250) + 10 * food.level + 100;
+		Misc3.drawCave();
+		IO.printMessage("A veil of darkness surrounds you.");
+		return true;
+	}
+	
+	private static boolean scarePlayer(InvenType food) {
+		Player.py.flags.afraid += Misc1.randomInt(10) + food.level;
+		IO.printMessage("You feel terrified!");
+		return true;
+	}
+	
+	private static boolean confusePlayer(InvenType food) {
+		Player.py.flags.confused += Misc1.randomInt(10) + food.level;
+		IO.printMessage("You feel drugged.");
+		return true;
+	}
+	
+	private static boolean hallucinatePlayer(InvenType food) {
+		Player.py.flags.imagine += Misc1.randomInt(200) + 25 * food.level + 200;
+		IO.printMessage("You feel drugged.");
+		return true;
+	}
+	
+	private static boolean cureConfusion() {
+		if (Player.py.flags.afraid > 1) {
+			Player.py.flags.afraid = 1;
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreStrength() {
+		if (Misc3.restoreStat(Constants.A_STR)) {
+			IO.printMessage("You feel your strength returning.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreConstitution() {
+		if (Misc3.restoreStat(Constants.A_CON)) {
+			IO.printMessage("You feel your health returning.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreIntelligence() {
+		if (Misc3.restoreStat(Constants.A_INT)) {
+			IO.printMessage("Your head spins a moment.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreWisdom() {
+		if (Misc3.restoreStat(Constants.A_WIS)) {
+			IO.printMessage("You feel your wisdom returning.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreDexterity() {
+		if (Misc3.restoreStat(Constants.A_DEX)) {
+			IO.printMessage("You feel more dextrous.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean restoreCharisma() {
+		if (Misc3.restoreStat(Constants.A_CHR)) {
+			IO.printMessage("Your skin stops itching.");
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean damagePlayer() {
+		Moria1.takeHit(Misc1.randomInt(18), "poisonous food.");
+		return true;
 	}
 }
